25e04925a9479d62c3fddb353621691de7033695
Michal Wereda <mwereda@future-processing.com>
2015-04-08 21:42:51 +0200

    Last failed build information
    Displaying div containing last faild build details

--- CINotifier/Scripts/app/notifier.viewmodel.js
+++ CINotifier/Scripts/app/notifier.viewmodel.js
@@ -1,5 +1,72 @@
 ﻿function notifierViewModel() {
     var self = this;
 
+    function getProject(name) {
+        var projects = self.projects().filter(function (project) {
+            return project.Project.Name === name;
+        });
+
+        return projects[0];
+    }
+
+    function getLastFailedFromProject(name) {
+        var project = getProject(name);
+        if (project) {
+            return project.Builds[0];
+        }
+
+        return null;
+    }
+
     self.projects = ko.observableArray([]);
+
+    self.isLastSuccess = ko.computed({
+        read: function () {
+            return true;
+        },
+        write: function (name) {
+            var project = getProject(name);
+            if (project) {
+                return project.IsLastSuccess;
+            }
+
+            return true;
+        },
+        owner: this
+    });
+
+    self.lastFailedDeveloper = ko.computed(
+    {
+        read: function () {
+            return '';
+        },
+        write: function (name) {
+            if (!self.isLastSuccess()) {
+                var failedBuild = getLastFailedFromProject(name);
+                if (failedBuild) {
+                    return failedBuild.Developer.Name;
+                }
+            }
+
+            return null;
+        },
+        owner: this
+    });
+
+    self.lastFailedVersion = ko.computed({
+        read: function () {
+            return '';
+        },
+        write: function (name) {
+            if (!self.isLastSuccess()) {
+                var failedBuild = getLastFailedFromProject(name);
+                if (failedBuild) {
+                    return failedBuild.Number;
+                }
+            }
+
+            return null;
+        },
+        owner: this
+    });
 }
