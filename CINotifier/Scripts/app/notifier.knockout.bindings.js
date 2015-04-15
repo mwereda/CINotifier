68a644c080260e0587fa2481cecec76efdfaa931
Michal Wereda <mwereda@future-processing.com>
2015-04-15 12:49:31 +0200

    Displaying build version in tooltip (builds' history)

---
+++ CINotifier/Scripts/app/notifier.knockout.bindings.js
@@ -0,0 +1,19 @@
+﻿ko.bindingHandlers.tooltip = {
+    init: function (element, valueAccessor) {
+        var local = ko.utils.unwrapObservable(valueAccessor()),
+            options = {};
+
+        ko.utils.extend(options, ko.bindingHandlers.tooltip.options);
+        ko.utils.extend(options, local);
+
+        $(element).tooltip(options);
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            $(element).tooltip("destroy");
+        });
+    },
+    options: {
+        placement: "right",
+        trigger: "click"
+    }
+};
